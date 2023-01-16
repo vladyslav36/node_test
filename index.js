79fcda8f657d3d6763a83c44b1b0f53021cda31b
vladyslav36 <vladyslav36@ukr.net>
2023-01-16 08:15:23 +0200

    16012023

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const MODE = process.env.MODE
 const app = express()
 app.use(express.json())
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({message:'hello'})
 })
 
