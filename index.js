b721ea6fcf3b7c9b81871e161b13c7326ba151a1
vladyslav36 <vladyslav36@ukr.net>
2023-01-22 08:35:50 +0200

    21012023

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.status(200).json({message:'main page cyclic'})
+  res.status(200).json({message:'main page '})
 })
 
 app.listen(PORT, () => {
